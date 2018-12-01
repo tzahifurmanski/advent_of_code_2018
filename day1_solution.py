97b1e7f68c7d017aa63ca56991076ff9a664cc04
Tzahi Furmanski <tzahi.fur@gmail.com>
2018-12-01 18:04:44 +0200

    V2

--- day1_solution.py
+++ day1_solution.py
@@ -1,27 +1,29 @@
-
 # Advent of code challenge - day 1
+# ================================
 
-summarizer = 0
+# Load the actions to memory
+actions_to_perform = []
+file = open("day1_input.txt", "r")
+for current_action in file:
+    actions_to_perform.append(int(current_action))
+file.close()
+
+# Initialize frequency related vars
+current_frequency = 0
 frequencies_history = set()
-frequencies_history.add(summarizer)
-
-file = open("input.txt", "r")
+frequencies_history.add(current_frequency)
 
 found = False
 while not found:
-    # Open the file and iterate everything
-    for line in file:
-        summarizer += int(line)
+    for current_action in actions_to_perform:
+        current_frequency += current_action
 
-        # Compare the size of the set before and after I add the current frequency
+        # Compare the size of the set before and after I add the current frequency (__len__() is O(1))
         len_before = frequencies_history.__len__()
-        frequencies_history.add(summarizer)
+        frequencies_history.add(current_frequency)
         len_after = frequencies_history.__len__()
         if len_before == len_after:
             # Size is the same, meaning the frequency was seen before
-            print("Frequency found! {}".format(summarizer))
+            print("Frequency found! {}".format(current_frequency))
             found = True
             break
-
-    # Reset the cursor to the beginning of the file so we can loop again
-    file.seek(0, 0)
